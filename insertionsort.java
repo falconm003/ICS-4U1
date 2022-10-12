927227cf09bd8e36e82f8f66e229f13cb6e9106e
falconm003 <113205053+falconm003@users.noreply.github.com>
2022-10-12 14:33:52 -0400

    2022-10-12

---
+++ insertionsort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class insertionsort {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] array = new int[5];
+        for(int ctr = 0; ctr < 5; ctr++) {
+            System.out.println("Enter number " + (ctr+1));
+            array[ctr] = s.nextInt();
+        }
+        insertionSorter(array);
+        System.out.println(Arrays.toString(array));
+        s.close();
+    }
+    public static void insertionSorter(int numList[]) {
+        int hold;
+        int ptr; 
+        for (int ctr = 1; ctr < numList.length; ctr++) {
+            hold = numList[ctr];
+            ptr = 0; 
+            while(hold > numList[ptr]) {
+                ptr++;
+            }
+            for (int count = ctr; count > ptr; count--) {
+                numList[count] = numList[count-1];
+            }
+            numList[ptr] = hold;
+        } 
+    }
+}
