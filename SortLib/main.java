5efc9dce7a09617c2eabc54eb3d8395471470f00
falconm003 <113205053+falconm003@users.noreply.github.com>
2022-10-18 01:17:32 -0400

    Delete SortLib directory

--- SortLib/main.java
+++
@@ -1,9 +0,0 @@
-class main {
-    public static void main(String[] args) throws InterruptedException {
-        int array[] = new int[10000];
-        SortLib sl = new SortLib();
-        sl.makeData(array);
-        Long time = sl.bubbleSorter(array);
-        System.out.println("BubbleSort took " + time + "ms.");
-    }
-}
