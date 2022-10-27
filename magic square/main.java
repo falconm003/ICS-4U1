d113538881dc09d06481cd754e3b29aedf8571f3
falconm003 <113205053+falconm003@users.noreply.github.com>
2022-10-27 16:03:02 -0400

    2022-10-27
    Every other revision was a lie. This is FINAL.

--- magic square/main.java
+++ magic square/main.java
@@ -1,3 +1,6 @@
+// Falcone, Matthew
+// Magic Square Problem Set
+// 2022-10-27
 import java.util.*;
 class Main {
     public static void printGrid(int[][] grid) {
@@ -15,6 +18,7 @@ class Main {
         int square = s.nextInt();
         int[][] grid = new int[square][square];
         magic(grid, square);
+        s.close();
     }
     public static void magic(int[][] grid, int square){
         Scanner s = new Scanner(System.in);
@@ -24,11 +28,12 @@ class Main {
                 grid[ctr][ctr2] = s.nextInt();
             }
         }
+        s.close();
         printGrid(grid);
         int[] Row = new int[square];
         int[] Column = new int[square];
-        int[] Diagonal1 = new int[square];
-        int[] Diagonal2 = new int[square];
+        int Diagonal1 = 0;
+        int Diagonal2 = 0;
         boolean hasRow = true;
         boolean hasColumn = true;
         boolean hasDiagonal1 = true;
@@ -40,11 +45,11 @@ class Main {
                 Row[ctr] += grid[ctr][ctr2];
             }
         }
-        for(int ctr = 0; ctr < Row.length; ctr++) {
-            if (Row[0] != Row[ctr]) {
-                hasRow = false;
-            }
-        }
+                            for(int ctr = 0; ctr < Row.length; ctr++) {
+                                if (Row[0] != Row[ctr]) {
+                                    hasRow = false;
+                                }
+                            }
 
         // column
         for(int ctr = 0; ctr < grid.length; ctr++) {
@@ -52,31 +57,31 @@ class Main {
                 Column[ctr] += grid[ctr2][ctr];
             }
         }
-        for(int ctr = 0; ctr < Column.length; ctr++) {
-            if (Column[0] != Column[ctr]) {
-                hasColumn = false;
-            }
-        }
+                            for(int ctr = 0; ctr < Column.length; ctr++) {
+                                if (Column[0] != Column[ctr]) {
+                                    hasColumn = false;
+                                }
+                            }
 
         // diagonal 1
         for(int ctr = 0; ctr < grid.length; ctr++) {
-            Diagonal1[ctr] += (grid[ctr][ctr]);
+            Diagonal1 += (grid[ctr][ctr]);
         }
-        for(int ctr = 0; ctr < Diagonal1.length; ctr++) {
-            if (Diagonal1[0] != Diagonal1[ctr]) {
-                hasDiagonal1 = false;
-            }
-        }
-        
+                            for(int ctr = 0; ctr < Row.length; ctr++) {
+                                if (Diagonal1 != Row[ctr] || Diagonal1 != Column[ctr]) {
+                                    hasDiagonal1 = false;
+                                }
+                            }
+                            
         // diagonal 2
         for(int ctr = (grid.length - 1); ctr >= 0; ctr--) {
-            Diagonal2[ctr] += (grid[ctr][((grid.length-1)-ctr)]);
+            Diagonal2 += (grid[ctr][((grid.length-1)-ctr)]);
         }
-        for(int ctr = 0; ctr < Diagonal2.length; ctr++) {
-            if (Diagonal2[0] != Diagonal2[ctr]) {
-                hasDiagonal2 = false;
-            }
-        }
+                            for(int ctr = 0; ctr < Row.length; ctr++) {
+                                if (Diagonal2 != Row[ctr] || Diagonal2 != Column[ctr]) {
+                                    hasDiagonal2 = false;
+                                }
+                            }
 
         if (hasRow && hasColumn && hasDiagonal1 && hasDiagonal2) {
             System.out.println("This is a magic square");
