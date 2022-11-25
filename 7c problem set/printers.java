b547dfe46e89dfe6bcc567366e5c5e75961cc852
falconm003 <113205053+falconm003@users.noreply.github.com>
2022-11-24 22:36:20 -0500

    Delete 7c problem set directory

--- 7c problem set/printers.java
+++
@@ -1,7 +0,0 @@
-public class printers {
-    public static void printTitle(Books[] book) {
-        for(int ctr = 0; ctr < book.length; ctr++) {
-            System.out.println((ctr+1) + ") " + book[ctr].getTitle());
-        }
-    }
-}
